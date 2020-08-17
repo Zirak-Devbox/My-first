ddab7aecbe5a7fc29af83e007a91455e3bc3a102
M. Zirak Waheed <zirak.waheed@devbox.co>
2020-08-17 17:52:39 +0500

    Make app production ready

--- Gemfile
+++ Gemfile
@@ -3,10 +3,13 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.1'
 
+group :production do
+  gem 'pg'
+end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~> 1.4'
+#gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,15 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+    validates :created_on, presence: true
+    validate :date_after_current
+
+    private
+        def date_after_current
+            if (!created_on.nil?)
+                
+                if (created_on > Time.new)
+                    errors.add(:created_on,"date is not come yet")
+            end
+        end
+    end
 end
