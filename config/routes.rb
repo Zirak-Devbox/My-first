88550ac654aae038365d33843450b97a271d2600
M. Zirak Waheed <zirak.waheed@devbox.co>
2020-08-20 12:20:30 +0500

    update profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'articles#home'
   get 'about', to: 'articles#about'
   resources :articles
+  resources :users
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
 end
