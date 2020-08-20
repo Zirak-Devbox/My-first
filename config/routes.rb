65895942d4016adc3774fb076754006da98275f0
M. Zirak Waheed <zirak.waheed@devbox.co>
2020-08-20 10:48:05 +0500

    create user action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'articles#home'
   get 'about', to: 'articles#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
 end
